b3a99560064973ec40fb1f285b039d94b307e8dc
xbrianlong <99936334+xbrianlong@users.noreply.github.com>
2022-06-22 17:10:27 +0800

    profile creation page (#8)
    * user creation works minus photo upload
    
    * added level input
    
    * image upload working
    
    * finished 'button' but avatar blinks when edit fast
    
    * fixed id of components
    
    * fixed flickering issue by changing url
    
    * added comments. current routing not perfect
    
    * run build warnings fixed

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,8 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import AuthProvider from '../contexts/AuthContext';
 import PrivateOutlet from'../components/PrivateOutlet';
 import Dashboard from './Dashboard';
-import Signup from "./Signup";
-import Login from "./Login";
+import Signup from "./Signup"
+import Login from "./Login"
+import ProfileCreation from './ProfileCreation';
 import ProfilePage from './ProfilePage';
 
 function App() {
@@ -14,7 +15,10 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route element={<PrivateOutlet />}>
+          {/* signup goes to create and login goes to slash */}
+          {/* possible that user skips create by closing window */}
           <Route path="/" element={<Dashboard />} />
+          <Route path="/create" element={<ProfileCreation />} />
           <Route path="/profile/:uid" element={<ProfilePage />} />
         </Route>
       </Routes>
