eb61b84c569b260343bca1251c1013431ab5e8fc
xbrianlong <brianlongjx@gmail.com>
2022-07-03 03:49:40 +0800

    routing with authentication + userData check works

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
-import { auth } from '../firebase'
+import { auth, db } from '../firebase'
+import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = React.createContext()
 
@@ -11,6 +12,8 @@ export function useAuth() {
 export default function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
     const [loading, setLoading] = useState(true)
+    const [ userData, setUserData ] = useState();
+
 
     function signup(email, password) {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -24,11 +27,40 @@ export default function AuthProvider({ children }) {
         return signOut(auth)
     }
     
-
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, user =>  {
+
             setCurrentUser(user)
-            setLoading(false)
+
+            const fetchData = async () => {
+                try {
+                    const docRef = doc(db, "users", user.uid);
+                    const docSnap = await getDoc(docRef);
+                    console.log("parsing data", user)
+                    if (docSnap.exists()) {
+                        console.log("done parsing");
+                        const docData = docSnap.data();
+                        console.log(docData);
+                        setUserData(docData);
+                        return docData;
+                    } else {
+                    // doc.data() will be undefined in this case
+                        console.log("Error! No such document!");
+                        setUserData(null);
+                        return null;
+                    }
+                // }
+                } catch (error) {
+                console.log(error);
+                }
+            }
+            const promisedData = fetchData();
+            // fetchData returns a promise which has to be resolved if not the routing will not work properly
+            promisedData.then((fetchedData) => {
+                setUserData(fetchedData);
+                console.log(fetchedData);
+            })
+            setLoading(false);
         })
 
         return unsubscribe
@@ -37,6 +69,8 @@ export default function AuthProvider({ children }) {
     const value = {
         currentUser,
         loading,
+        userData,
+        setUserData,
         signup,
         login,
         logout
